2ec9ce7eb25ae02fa42b95e22f3ebe4bb2fcb86e
Benjamin Wilde <bendavidwilde95@gmail.com>
2021-08-06 12:44:11 -0500

    added while loop that runs as long as user hasnt guessed all 50 states

--- app.py
+++ app.py
@@ -207,29 +207,34 @@ correct = 0
 incorrect = 0
 tries = 0
 
-print('Welcome to the game where you guess the state capitals!')
-welcome_question = input('Do you want to begin? [y/n] ').lower()
+while tries < 51:
 
-if welcome_question == 'y':
-    random.shuffle(state_capitals)
-    
-    for state in state_capitals:
-        question = input(f'What is the state capital of {state["name"]}? ').title()
-        if question == state["capital"]:
-            print(f'Correct! You guessed {question}')
-            tries += 1
-            correct += 1
-            print(f'You have correctly guessed {correct} states')
-            print(f'You have incorrectly guessed {incorrect} states')
-            print(f'You have guessed {tries} total times')
-            
-        else: 
-            print(f'Sadly, {question} is incorrect. Try again!')
-            incorrect += 1
-            tries += 1
-            print(f'You have correctly guessed {correct} states')
-            print(f'You have incorrectly guessed {incorrect} states')
-            print(f'You have guessed {tries} total times')
-            
-else:
-    print('You are missing out')
+    if tries == 50:
+        print(f'You have gone through all the states, the game is now over. Your final score was {correct} correct, and {incorrect} incorrect')
+
+    print('Welcome to the game where you guess the state capitals!')
+    welcome_question = input('Do you want to begin? [y/n] ').lower()
+
+    if welcome_question == 'y':
+        random.shuffle(state_capitals)
+        
+        for state in state_capitals:
+            question = input(f'What is the state capital of {state["name"]}? ').title()
+            if question == state["capital"]:
+                print(f'Correct! You guessed {question}')
+                tries += 1
+                correct += 1
+                print(f'You have correctly guessed {correct} states')
+                print(f'You have incorrectly guessed {incorrect} states')
+                print(f'You have guessed {tries} total times')
+                
+            else: 
+                print(f'Sadly, {question} is incorrect. Try again!')
+                incorrect += 1
+                tries += 1
+                print(f'You have correctly guessed {correct} states')
+                print(f'You have incorrectly guessed {incorrect} states')
+                print(f'You have guessed {tries} total times')
+                
+    else:
+        print('You are missing out')
